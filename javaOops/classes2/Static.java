bcaf117791290128f63f516a612f61cf48dd92e7
shivam_kumar <shivamkumar14046@gmail.com>
2026-02-21 22:10:53 +0530

    use of static keyword

---
+++ javaOops/classes2/Static.java
@@ -0,0 +1,44 @@
+package javaOops.classes2;
+
+class UseStatic {
+    static int a = 3;
+    static int b;
+
+    static void meth(int x) {
+        System.out.println("x = " + x);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+    static {
+        System.out.println("Static block initialized. ");
+        b = a * 4;
+    }
+
+    int d = 1;
+
+    public static void main(String[] args) {
+        meth(42);
+        // System.out.println(d); give error
+    }
+}
+
+class StaticDemo {
+    static int a = 42;
+    static int b = 99;
+
+    static void callme() {
+        System.out.println("a = " + a);
+    }
+}
+
+public class Static {
+    public static void main(String[] args) {
+        // System.out.println(ob.a); give error
+        UseStatic.meth(4);
+        System.out.println(UseStatic.a);
+
+        StaticDemo.callme();
+        System.out.println("b = " + StaticDemo.b);
+    }
+}
